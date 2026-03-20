5ab5eb96742322c9792b865cbb92b903e11fe0e3
Thomas.Sutil <thomas.sutil@walibi.com>
2026-03-20 22:51:28 +0100

    v3.0 Add Test with BUNIT

---
+++ MyAppBlazorThomas.Tests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Bunit;
+using MyAppBlazorThomas.Pages;
+using Xunit;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+
+public class LoginTests
+{
+    [Fact]
+    public void Login_AfficheDeuxInputsEtUnBouton()
+    {
+        using var ctx = new BunitContext();
+        var cut = ctx.Render<Login>();
+
+        Assert.Equal(2, cut.FindAll("input").Count);
+        Assert.Contains("Login", cut.Markup);
+    }
+    [Fact]
+    public void Login_BonIdentifiants_NavigueVersKaamelott()
+    {
+        using var ctx = new BunitContext();
+        var nav = ctx.Services.GetRequiredService<NavigationManager>();
+        nav.NavigateTo("http://localhost/login");
+
+        var cut = ctx.Render<Login>();
+
+        cut.Find("input[placeholder='Login']").Change("admin");
+        cut.Find("input[type='password']").Change("1234");
+
+        cut.Find("form").Submit();
+
+        Assert.EndsWith("/kaamelott", nav.Uri);
+    }
+    [Fact]
+    public void Login_PasswordInput_EstDeTypePassword()
+    {
+        using var ctx = new BunitContext();
+        var cut = ctx.Render<Login>();
+
+        var passwordInput = cut.Find("input[type='password']");
+        Assert.NotNull(passwordInput);
+    }
+}
